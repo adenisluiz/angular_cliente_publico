fde930e0d3d2f302aed580bd10a5312ab30e1782
pc <adenis.luiz@hotmail.com>
2023-11-03 19:15:44 -0300

    diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirtComponentComponent } from './components/firt-component/firt-component.component';
 import { TesteComponent } from './components/teste/teste.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DiretivasComponent } from './components/diretivas/diretivas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirtComponentComponent,
     TesteComponent,
-    ParentDataComponent
+    ParentDataComponent,
+    DiretivasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/diretivas/diretivas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas',
+  templateUrl: './diretivas.component.html',
+  styleUrls: ['./diretivas.component.css']
+})
+export class DiretivasComponent {
+size=12;
+colors="red";
+myClass="my-class"
+
+}

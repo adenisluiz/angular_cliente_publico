5208a583d6d72ede1fcf8d30997a603bf3823e60
pc <adenis.luiz@hotmail.com>
2023-11-02 21:35:22 -0300

    Primeiro componente aula 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirtComponentComponent } from './components/firt-component/firt-component.component';
+import { TesteComponent } from './components/teste/teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirtComponentComponent
+    FirtComponentComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
